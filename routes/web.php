4f0fbe286b6d343179bf640d6ad59928093494a3
Szczepan <szczep.kowalik@gmail.com>
2022-06-23 21:51:13 +0200

    route for ExampleController

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\ExampleController;
 
 Route::get('/', [MainController::class, 'index'])->name('main');
+Route::get('/show', [ExampleController::class, 'index'])->name('example.index');
 
 Route::POST('/', [MainController::class, 'store'])->name("main.store");
