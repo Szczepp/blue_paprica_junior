a2ac38bf6f0d23404401e73b477f09a480ba425c
Szczepan <szczep.kowalik@gmail.com>
2022-06-23 21:48:29 +0200

    Example model config

--- app/Models/Example.php
+++ app/Models/Example.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Example extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'surname',
+        'image'
+    ];
+
+    public function setImageAttribute($image)
+    {
+        $this->attributes['path'] = $image;
+    }
 }
