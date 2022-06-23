65bdffe3119e8eec7de8af258b503845bb19ad4d
Szczepan <szczep.kowalik@gmail.com>
2022-06-23 21:50:21 +0200

    ExampleController to show example table content

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Example;
+use Illuminate\Http\Request;
+
+class ExampleController extends Controller
+{
+    public function index()
+    {
+        return view('show')->with([
+            'examples' => Example::all()
+        ]);
+    }
+}
