e6f934d2708b297ceed4512b79071a0f4bcddbdc
Szczepan <szczep.kowalik@gmail.com>
2022-06-24 14:08:10 +0200

    set 'path' nullable

--- database/migrations/2022_06_23_154435_create_examples_table.php
+++ database/migrations/2022_06_23_154435_create_examples_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->text('name');
             $table->text('surname');
-            $table->text('path')->default('-');
+            $table->text('path')->nullable();
 
             $table->timestamps();
         });
