afd7119630c2c6e23693c1f476fdbfbe1ef7d470
Szczepan <szczep.kowalik@gmail.com>
2022-06-25 12:26:20 +0200

    Only authenticated users can see the content

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class ExampleController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         return view('show')->with([
